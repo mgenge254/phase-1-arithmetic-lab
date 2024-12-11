560c9d306843be5405d3f38ecf9f7d57f8ecf84d
mgenge254 <jeremiah.orodi@student.moringaschool.com>
2024-12-11 10:49:57 +0300

    newcommit

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-
-// Write your code here
+let num1 = 31
+let num2 = 2
+let multiply = num1*num2
+const random = Math.floor(Math.random() * 100) + 1;
+const num3 = 10;
+const num4 = 6;
+const mod = num3 % num4;
+const max = Math.max(5, 10, 15, 20);
